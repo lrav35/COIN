5d8f1371390f2499138ce1fcaf18d988a002fb55
Luc Ravenelle <Luc.Ravenelle@libertymutual.com>
2021-05-03 22:53:23 -0400

    add interfece

--- COIN.sol
+++ COIN.sol
@@ -4,15 +4,11 @@ pragma solidity 0.6.6;
 // 'COIN' token contract
 //
 // Deployed to :
-// Symbol      : COI
-// Name        : COINToken
+// Symbol      : COIN
+// Name        : COIN Token
 // Total supply: 100000000
 // Decimals    : 18
 //
-// ----------------------------------------------------------------------------
-
-// ----------------------------------------------------------------------------
-// Safe math
 // ----------------------------------------------------------------------------
 
 contract SafeMath {
@@ -37,4 +33,43 @@ contract SafeMath {
         c = a / b;
     }
 
+}
+
+
+//Implement the ERC Token Standard Interface
+
+abstract contract ERC20Interface {
+      function totalSupply() virtual public view returns (uint balance);
+      function balanceOf(address owner) virtual public view returns (uint remaining);
+      function allowance(address owner, address spender) virtual public view returns (uint remaining);
+      function transfer(address to, uint tokens) virtual public view returns (bool success);
+      function approve(address spender, uint tokens) virtual public view returns (bool success);
+      function transferFrom(address from, address to, uint tokens) virtual public view returns (bool success);
+
+      event Transfer(address indexed from, address indexed to, uint tokens);
+      event Approval(address indexed owner, address indexed spender, uint tokens);
+}
+
+// Contract to receive approval and execute function in one call
+
+abstract contract ApproveAndCallFallBack {
+      function receiveApproval(address from, uint256 tokens, address token, bytes memory data) virtual public;
+}
+
+// Ownership
+
+contract Owned {
+      address public owner;
+      address public newOwner;
+
+      event OwnershipTransferred(address indexed _from, address indexed _to);
+
+      constructor() public {
+          owner = msg.sender;
+      }
+
+      modifier onlyOwner {
+          require(msg.sender == owner);
+          _;
+      }
 }
